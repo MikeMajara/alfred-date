e448be4728c1b8362daf8ab3c2566f91dc9ccafe
mikemajara <fmiguel05@gmail.com>
2020-05-16 18:00:05 +0200

    Latest working wf

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-# This is not the script being executed. this is just a script
-# to test and ease the development of the workflow. Code being
-# executed is stored in info.plist and should be modified through
+# This script is duplicated and functionality is implemented for
+# both locigs: (with | without) language. Logic should be common
+# and only change is LANGUAGE_PROVIDED.
+# 
+# $WORKFLOW_DIRECTORY/main.py is not the script being executed. 
+# it is just a script to test and ease the development of the workflow. 
+# Code being executed is stored in info.plist and should be modified through
 # alfred to take effect.
+# 
+# important: print nothing else to STDOUT, or it will make your XML/JSON invalid.
 # 
 # Useful documentation: https://www.alfredapp.com/help/workflows/inputs/script-filter/json/
 # date format patterns: https://unicode.org/reports/tr35/tr35-dates.html#Date_Format_Patterns
 # Babel documentation: http://babel.pocoo.org/en/latest/dates.html?highlight=pattern#pattern-syntax
 
-
-import os
-import sys
+import os, sys
+import traceback
 import json
 
 # add local library path to python path.
@@ -18,36 +23,69 @@ import json
 # install command example (cd to workflow directory):
 # pip install --prefer-binary --target=./lib dateparser
 sys.path = [os.path.abspath('./lib')] + sys.path
+
 import dateparser
 from babel.dates import format_datetime
 
-# Get argument
-query = sys.argv[1]
+LANGUAGE_PROVIDED = True
 
-# Start your script
-# important: It's important that you print nothing else to STDOUT, or it will make your XML/JSON invalid.
-
+# Extract arguments from script
+args = sys.argv[1].split()
 
 with open("./config.json") as f:
     config = json.load(f)
 
+with open("./locales.json") as f:
+    locales = json.load(f)
+
 fmts = config['formats']
 lngs = config['languages']
-output_language = config['output_language']
 
+if LANGUAGE_PROVIDED:
+    query = " ".join(args[1:])
+    out_lang = args[0].strip()
+    if len(out_lang) == 2:
+        name = locales[out_lang].get('name')
+        format = locales[out_lang].get('default_format')
+        out_lang = locales[out_lang].get('locale')
+else:
+    query = " ".join(args)
+    out_lang = config['out_lang']
+    
 dt = dateparser.parse(query, languages=lngs)
 
-items = [
-    {
-        "valid": False,
-        "icon": None,
-        "title": format_datetime(dt, format=fmt['format_string'], locale=output_language),
-        "subtitle": fmt['format_string'],
-        "text": {
-            "copy": format_datetime(dt, format=fmt['format_string'], locale=output_language),
-            "largetype": format_datetime(dt, format=fmt['format_string'], locale=output_language)
-        }
-    } for fmt in fmts
-]
+result = {"items": []}
+try:
+    default_items = [
+        {
+            "valid": False,
+            "icon": None,
+            "title": fmt['format_string'],
+            "subtitle": format_datetime(dt, format=fmt['format_string'], locale=out_lang),
+            "text": {
+                "copy": format_datetime(dt, format=fmt['format_string'], locale=out_lang),
+                "largetype": format_datetime(dt, format=fmt['format_string'], locale=out_lang)
+            }
+        } for fmt in fmts
+    ]
+    result['items'] += default_items
 
-sys.stdout.write(json.dumps({"items":items}))
+    if LANGUAGE_PROVIDED:
+
+        extra_item = {
+            "valid": False,
+            "icon": None,
+            "title": format_datetime(dt, format=format, locale=out_lang),
+            "subtitle": format + " - Default for " + name,
+            "text": {
+                "copy": format_datetime(dt, format=format, locale=out_lang),
+                "largetype": format_datetime(dt, format=format, locale=out_lang)
+            }
+        }
+        result['items'] += [extra_item]
+
+except Exception as e:
+    traceback.print_exc()
+    # pass
+
+sys.stdout.write(json.dumps(result))
